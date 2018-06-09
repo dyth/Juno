640fde6f2941b538273a5a8f679204e150509144
dyth <dythui1@gmail.com>
2018-06-09 23:02:13 +0100

    rudimentary user interface

--- test.py
+++ test.py
@@ -5,6 +5,36 @@ from value_network import *
 from noughts_crosses import *
 
 
+class user:
+
+    def pretty_print(self, board):
+        printBoard = []
+        for i in range(9):
+            if board[i] == players[0]:
+                printBoard.append("X")
+            elif board[i] == players[1]:
+                printBoard.append("O")
+            else:
+                printBoard.append("\033[93m" + str(i+1) + "\033[0;0m")
+        print """
+   |   |
+ %s | %s | %s
+___|___|___
+   |   |
+ %s | %s | %s
+___|___|___
+   |   |
+ %s | %s | %s
+   |   |
+""" % tuple(printBoard)
+    
+    def minimax(self, board, player):
+        self.pretty_print(board)
+        index = input("Type the number of the square to move ")
+        board[index-1] = player
+        return board
+
+
 def self_play(engines):
     'engines is a list of engines and engines[0] moves first'
     board = initialBoard
@@ -14,14 +44,14 @@ def self_play(engines):
         board = engines[index].minimax(board, player)
         player = next_player(player)
         index = int(not index)
-        pretty_print(board)
     return player
 
 
 if __name__ == "__main__":
     e = Engine(optimal, 9)
-    ran = Engine(random, 1)
-    self_play([e, ran])
+    #ran = Engine(random, 1)
+    u = user()
+    self_play([u, e])
     #print e.minimax(initialBoard, players[0])
     #v = ValueNet()
     #e = Engine(v, 2)
