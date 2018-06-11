cd58938a0cef51ce78122d596c0eb14482b37975
dyth <dythui1@gmail.com>
2018-06-11 22:40:55 +0100

    eligibility trace

--- value_network.py
+++ value_network.py
@@ -16,11 +16,13 @@ class ValueNet(nn.Module):
     """
     Value Network Layers, Architecture and forward pass
     """
-    def __init__(self):
+    def __init__(self, learningRate, decay):
         'initialise all the layers and activation functions needed'
         super(ValueNet, self).__init__()
 
+        self.learningRate = learningRate
         self.weightsNum = 0
+        self.decay = decay
         
         # three layers
         self.fc1 = nn.Linear(9, 64)
@@ -41,18 +43,22 @@ class ValueNet(nn.Module):
             inputLayer = inputLayer.cuda()
         return Variable(inputLayer)
     
-    
-    def forward(self, inputLayer):
+
+    def forward_pass(self, inputLayer):
         'forward pass using Variable inputLayer'
-        inputLayer = self.list_to_Variable(inputLayer)
         out = self.fc1(inputLayer)
         out = F.relu(out)
         out = self.fc2(out)
         out = F.relu(out)
         out = self.fc3(out)
-        out = F.tanh(out)
-        return out.data[0]
-
+        return F.tanh(out)
+    
+    
+    def forward(self, inputLayer):
+        'forward pass using Variable inputLayer'
+        inputLayer = self.list_to_Variable(inputLayer)
+        return self.forward_pass(inputLayer).data[0]
+    
 
     def load_weights(self):
         'load name'
@@ -60,7 +66,25 @@ class ValueNet(nn.Module):
     
     
     def save_weights(self, directory):
-        'save the weights as the number in directory/'
+        'save the weights as the number in "directory/"'
         self.weightsNum += 1
         name = directory + "/" +  str(self.weightsNum) + ".h5"
         torch.save(self.state_dict(), name)
+
+    
+    def temporal_difference(self, boards, lastValue):
+        'backup values according to boards'
+        traces, gradients, trace = [], [], 0.0
+        # boards goes forward in time, so reverse index
+        for i in range(len(boards)-1, -1, -1):
+            # forward pass
+            board = self.list_to_Variable(boards[i])
+            value = self.forward_pass(board)
+            # compute eligibility trace
+            trace = trace * self.decay + (lastValue - value)
+            traces.append(trace)
+            lastValue = value
+            # autograd of gradient
+            value.backward()
+            gradients.append(b.grad)
+        
